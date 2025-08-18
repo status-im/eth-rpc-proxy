7d6e8c2f8450c10a6dcdc4cceddf68aa54428487
Andrey Bocharnikov <andrey.bocharnikov@gmail.com>
2025-08-18 22:19:07 +0400

    feat(go-proxy-cache)_: fix keybuilder.go

--- go-proxy-cache/internal/interfaces/keybuilder.go
+++ go-proxy-cache/internal/interfaces/keybuilder.go
@@ -7,7 +7,7 @@ import "go-proxy-cache/internal/models"
 // KeyBuilder canonizes requests into deterministic cache keys
 type KeyBuilder interface {
 	// For single request
-	Build(chain string, network string, req *models.JSONRPCRequest) (key string, paramsHash uint32)
+	Build(chain string, network string, req *models.JSONRPCRequest) (string, error)
 	// For batch, returns per-item keys aligned by index
-	BuildBatch(chain, network string, reqs []models.JSONRPCRequest) ([]string, []uint32)
+	BuildBatch(chain, network string, reqs []models.JSONRPCRequest) ([]string, error)
 }
